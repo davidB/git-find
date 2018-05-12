5a1bfe6a15b2a4028e2b13f790ec4cead09b8c5c
David Bernard <david.bernard.31@gmail.com>
2018-05-12 12:25:26 +0200

    :building_construction: :heavy_plus_sign: use slog for logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+#[macro_use]
+pub extern crate slog;
+
+pub struct Ctx {
+    pub logger: slog::Logger,
+}
+
 pub struct GitRepo {}
 
-pub fn find_repos(root: &str) -> Vec<GitRepo> {
+pub fn find_repos(ctx: &Ctx, root: &str) -> Vec<GitRepo> {
     unimplemented!()
 }
 
-pub fn render(tmpl: &str, value: &GitRepo) -> String {
+pub fn render(ctx: &Ctx, tmpl: &str, value: &GitRepo) -> String {
     unimplemented!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 extern crate git_find;
+#[macro_use]
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
 
 use git_find::*;
+use slog::Drain;
+
+fn init_log() -> slog::Logger {
+    let drain = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(drain).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = slog::Logger::root(drain, o!());
+    info!(log, "start"; "version" => env!("CARGO_PKG_VERSION"));
+    log
+}
 
 fn main() {
+    let log = init_log();
+
     let root = ".";
     let tmpl = "{{.}}";
-    find_repos(root)
+    let ctx = Ctx { logger: log };
+    find_repos(&ctx, root)
         .iter()
-        .map(|r| render(tmpl, r))
+        .map(|r| render(&ctx, tmpl, r))
         .for_each(|s| println!("{}", s));
 }
